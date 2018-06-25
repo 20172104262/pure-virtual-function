c21b630d3d216d53e08f805a39a4400518c0c0a4
20172104262 <376261470@qq.com>
2018-06-25 10:13:33 +0800

    Initial commit

---
+++ pure virtual function/pure virtual function.cpp
@@ -0,0 +1,42 @@
+// pure virtual function.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include"iostream"
+using namespace std;
+
+class Cbase
+{
+public:
+	virtual void test() = 0;
+	virtual void display()
+	{
+		cout << "基类" << endl;
+	}
+};
+class Cderivd :public Cbase
+{
+public:
+	void test()
+	{
+
+	 }
+	void display()
+	{
+		cout << "派生类" << endl;
+	}
+};
+
+int main()
+{
+	Cbase *q;
+	//Cbase obj1;
+	Cderivd obj2;
+	//obj1.display();
+	q = &obj2;
+	q->Cbase::display();
+	q->display();
+	obj2.display();
+    return 0;
+}
+
